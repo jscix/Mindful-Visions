7ee9b0c3cfc149b629c9b0df54cdd82c2fb51bc2
Jesse <jscix1@gmail.com>
2012-03-26 20:51:31 -0400

    new classes to replace generic 'General" class

---
+++ MindfulVisions/ApplicationPaths.cs
@@ -0,0 +1,28 @@
+﻿namespace MindfulVisions
+{
+    using System.Diagnostics;
+    using System.Windows.Forms;
+
+    public class ApplicationPaths
+    {
+        public string RootDirectory()
+        {
+            return isDebugMode() ? @"C:\Users\Jesse\Documents\Visual Studio 2010\Projects\MindfulVisions\MindfulVisions\" : Application.StartupPath;
+        }
+
+        public string SoundDirectory()
+        {
+            string soundspath = RootDirectory();
+            if (soundspath.Substring((soundspath.Length - 1), 1) == @"\")
+            {
+                return soundspath + @"Sounds\";
+            }
+            return soundspath + @"\Sounds\";
+        }
+
+        private bool isDebugMode()
+        {
+            return Debugger.IsAttached;
+        }
+    }
+}
---
+++ MindfulVisions/Sounds.cs
@@ -0,0 +1,24 @@
+﻿using MindfulVisions.Properties;
+
+namespace MindfulVisions
+{
+    using System.Windows.Forms;
+    using System.Linq;
+
+    public class Sounds
+    {
+        public bool ValidateSoundList(ListView list)
+        {
+            return list.Items.Cast<ListViewItem>().Any(item => item.Checked);
+        }
+
+        public bool ValidateSoundList()
+        {
+            if (Settings.Default.Sounds.Count == 0 || Settings.Default.Sounds == null)
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
